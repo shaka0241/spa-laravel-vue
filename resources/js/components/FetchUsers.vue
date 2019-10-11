4dede4f7689bc6e92fedb506a6695b291fe21c97
shaka0241 <shaka0241@gmail.com>
2019-10-11 14:36:41 -0300

    primer commit / cosas hechas

---
+++ resources/js/components/FetchUsers.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		Obteniendo data via Api...
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+	mounted () {
+		axios.get('/api/users')
+		.then(response => console.log(response.data)) 
+	}
+}
+	
+</script>
